51325180d6d9710f5467b1c93df67b70ff81e3e6
Aryatama <18211338+aryatama045@users.noreply.github.com>
2025-04-29 09:32:46 +0700

    Update Common_model.php

--- application/models/Common_model.php
+++ application/models/Common_model.php
@@ -11,7 +11,7 @@ class Common_model extends CI_Model {
     {
 		$sql = "select * from ".$tbl;
 		if($options != NULL)
-		{ 
+		{
 			if(array_key_exists('where',$options))
 			{
 				$where = array($options['where']);
@@ -24,11 +24,10 @@ class Common_model extends CI_Model {
 					{
 						$sql .= ($once) ?  " $k = '$v'" : " and $k = '$v'";	
 						$once=0;
-					}	
+					}
 				}
 			}
-	
-	
+
 			if(array_key_exists('orderby',$options))
 			{
 				$orderby = array($options['orderby']);
@@ -39,37 +38,37 @@ class Common_model extends CI_Model {
 					$once=1;
 					foreach($orderby as $k =>$v)
 					{
-						$sql .= ($once) ?  " $k $v" : " , $k  $v ";	
+						$sql .= ($once) ?  " $k $v" : " , $k  $v ";
 						$once=0;
-					}	
+					}
 				}
 			}
 		}
 		$query = $this->db->query($sql);
-		return $query; 
+		return $query;
     }
-	
+
 	function commonSelect2($options)
     {
 
 		if(array_key_exists('from',$options))
 			$this->db->from($options['from']);
-		
+
 		if(array_key_exists('where',$options))
 			$this->db->where($options['where']);
-			
+
 		if(array_key_exists('orderby',$options))
 			$this->db->order_by($options['orderby']);
-		
+
 		$query = $this->db->get();
-		return $query; 
+		return $query;
     }
-	
+
 	function commonSelect1($tbl ,$options = NULL)
     {
 		$sql = "select * from ".$tbl;
 		if($options != NULL)
-		{ 
+		{
 			if(array_key_exists('where',$options))
 			{
 				$where = array($options['where']);
@@ -80,13 +79,12 @@ class Common_model extends CI_Model {
 					$once=1;
 					foreach($where as $k =>$v)
 					{
-						$sql .= ($once) ?  " $k = '$v'" : " and $k = '$v'";	
+						$sql .= ($once) ?  " $k = '$v'" : " and $k = '$v'";
 						$once=0;
-					}	
+					}
 				}
 			}
-	
-	
+
 			if(array_key_exists('orderby',$options))
 			{
 				$orderby = array($options['orderby']);
@@ -97,55 +95,56 @@ class Common_model extends CI_Model {
 					$once=1;
 					foreach($orderby as $k =>$v)
 					{
-						$sql .= ($once) ?  " $k $v" : " , $k  $v ";	
+						$sql .= ($once) ?  " $k $v" : " , $k  $v ";
 						$once=0;
-					}	
+					}
 				}
 			}
 		}
 		$query = $this->db->query($sql);
-		return $query; 
+		return $query;
     }
 
 	function commonQuery($sql)
     {
-		$query = $this->db->query($sql);	
-	    return $query; 
+		$query = $this->db->query($sql);
+		return $query;
     }
 
     function add_question()
     {
 		$user_id =1;
-       $data = array(
-	   'subject_id' => $_POST['subject'],'quest_title' => $_POST['question'],'quest_date' => time(),  
-	   'quest_status' => 'Y','visibility' => $_POST['visibility'],'quest_author' => $user_id  );
-		$this->db->insert('questions', $data); 
-		
+		$data = array(
+		'subject_id' => $_POST['subject'],'quest_title' => $_POST['question'],'quest_date' => time(),
+		'quest_status' => 'Y','visibility' => $_POST['visibility'],'quest_author' => $user_id  );
+		$this->db->insert('questions', $data);
+
 		$quest_id=$this->db->insert_id();
-		
+
 		$data = array( 'quest_id' => $quest_id,  'ans_desc' => $_POST['answer1'], 'ans_status' => 'Y', 'is_correct' => 'Y' );
-		$this->db->insert('answers', $data); 
-		
+		$this->db->insert('answers', $data);
+
 		$data = array( 'quest_id' => $quest_id,  'ans_desc' => $_POST['answer2'], 'ans_status' => 'Y', 'is_correct' => 'N' );
-		$this->db->insert('answers', $data); 
-		
+		$this->db->insert('answers', $data);
+
 		$data = array( 'quest_id' => $quest_id,  'ans_desc' => $_POST['answer3'], 'ans_status' => 'Y', 'is_correct' => 'N' );
-		$this->db->insert('answers', $data); 
-		
+		$this->db->insert('answers', $data);
+
 		$data = array( 'quest_id' => $quest_id,  'ans_desc' => $_POST['answer4'], 'ans_status' => 'Y', 'is_correct' => 'N' );
-		$this->db->insert('answers', $data); 
-		
+		$this->db->insert('answers', $data);
+
     }
 
 	function commonInsert($tbl,$data)
     {
-		$this->db->insert($tbl, $data); 
+		$this->db->insert($tbl, $data);
 		return $this->db->insert_id();
     }
+
 	function commonUpdate($tbl,$data,$fld,$id)
     {
 		$this->db->where($fld, $id);
-		$this->db->update($tbl, $data); 
+		$this->db->update($tbl, $data);
 		return $this->db->affected_rows();
     }
 
@@ -155,6 +154,7 @@ class Common_model extends CI_Model {
 		$this->db->delete($tbl);
 		return $this->db->affected_rows();
 	}
+
 	function commonbulkstatus($tbl,$rowid,$fld,$status)
 	{
 		$data = array('status' => $status);
@@ -163,15 +163,13 @@ class Common_model extends CI_Model {
 		return $this->db->affected_rows();
 	}
 
-	
-
 	function show_all_questions($whr = NULL,$pager = false)
     {
 		$sql = "select qs.*,cat.cat_name as subject ,users.user_name author	from questions qs
 				inner join category cat on cat.cat_id=qs.subject_id
 				inner join users on users.user_id = qs.quest_author	";
 		if($whr != NULL)
-		{ 
+		{
 			if(count($whr) > 0)
 			{
 				$sql .= " where ";
@@ -180,11 +178,11 @@ class Common_model extends CI_Model {
 				{
 					$sql .= ($once) ?  " $k = '$v'" : " and $k = '$v'";	
 					$once=0;
-				}	
+				}
 			}
 		}
 		$query = $this->db->query($sql);
-		return $query; 
+		return $query;
     }
 
 	function show_quest_subjects()
@@ -192,8 +190,8 @@ class Common_model extends CI_Model {
 		$sql = "select cat.cat_id, cat.cat_name as subject from questions qs
 				inner join category cat on cat.cat_id=qs.subject_id
 				group by cat.cat_id ";
-		$query = $this->db->query($sql);	
-	    return $query; 
+		$query = $this->db->query($sql);
+		return $query;
     }
 
 	function show_quest_dates()
@@ -202,19 +200,20 @@ class Common_model extends CI_Model {
 				from questions qs
 				inner join category cat on cat.cat_id=qs.subject_id
 				group by newdate ";
-		$query = $this->db->query($sql);	
-	    return $query; 
+		$query = $this->db->query($sql);
+		return $query;
     }
 
 	function show_quest_authors()
-    {
+	{
 		$sql = "select qs.quest_author	,users.user_name author
 				from questions qs
 				inner join users on users.user_id = qs.quest_author
 				group by qs.quest_author ";
-		$query = $this->db->query($sql);	
-	    return $query; 
-    }
+		$query = $this->db->query($sql);
+		return $query;
+	}
+
 	function set_status($qid,$task)
 	{
 		$data = array('quest_status' => $task);
@@ -222,6 +221,7 @@ class Common_model extends CI_Model {
 		$this->db->update('questions', $data );
 		return $this->db->affected_rows();
 	}
+
 	function set_bulkstatus($qid,$task)
 	{
 		$data = array('quest_status' => $task);
